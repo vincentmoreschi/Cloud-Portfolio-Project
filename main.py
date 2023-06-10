745843c8b3a30741139c46ef0367d2707503ab7c
Vincent Moreschi <vmzcon@gmail.com>
2023-06-10 12:33:43 -0400

    Working jwt and auth

---
+++ main.py
@@ -0,0 +1,169 @@
+from google.cloud import datastore
+from flask import Flask, request, jsonify, _request_ctx_stack
+import requests
+
+from functools import wraps
+import json
+
+from six.moves.urllib.request import urlopen
+from flask_cors import cross_origin
+from jose import jwt
+
+
+import json
+from os import environ as env
+from werkzeug.exceptions import HTTPException
+import logging
+
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask
+from flask import jsonify
+from flask import redirect
+from flask import render_template
+from flask import session
+from flask import url_for
+from authlib.integrations.flask_client import OAuth
+from six.moves.urllib.parse import urlencode
+import key
+
+app = Flask(__name__)
+client = datastore.Client()
+app.secret_key = key.key
+FOOD = "food"
+# Default Route
+
+# Update the values of the following 3 variables
+CLIENT_ID = 'uBmZb7sNadlurP5JTpdYh0H7ACnsE3AE'
+CLIENT_SECRET = key.key
+DOMAIN = 'dev-j6xiyw1cjonbunvv.us.auth0.com'
+# For example
+# DOMAIN = 'fall21.us.auth0.com'
+
+ALGORITHMS = ["RS256"]
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    api_base_url="https://" + DOMAIN,
+    access_token_url="https://" + DOMAIN + "/oauth/token",
+    authorize_url="https://" + DOMAIN + "/authorize",
+    client_kwargs={
+        'scope': 'openid profile email',
+    },
+    server_metadata_url=f'https://{DOMAIN}/.well-known/openid-configuration'
+
+)
+
+# This code is adapted from https://auth0.com/docs/quickstart/backend/python/01-authorization?_ga=2.46956069.349333901.1589042886-466012638.1589042885#create-the-jwt-validation-decorator
+
+class AuthError(Exception):
+    def __init__(self, error, status_code):
+        self.error = error
+        self.status_code = status_code
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+# Verify the JWT in the request's Authorization header
+def verify_jwt(request):
+    if 'Authorization' in request.headers:
+        auth_header = request.headers['Authorization'].split()
+        token = auth_header[1]
+    else:
+        return None
+    
+    jsonurl = urlopen("https://"+ DOMAIN+"/.well-known/jwks.json")
+    jwks = json.loads(jsonurl.read())
+    try:
+        unverified_header = jwt.get_unverified_header(token)
+    except jwt.JWTError:
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Invalid header. "
+                            "Use an RS256 signed JWT Access Token"}, 401)
+    if unverified_header["alg"] == "HS256":
+        raise AuthError({"code": "invalid_header",
+                        "description":
+                            "Invalid header. "
+                            "Use an RS256 signed JWT Access Token"}, 401)
+    rsa_key = {}
+    for key in jwks["keys"]:
+        if key["kid"] == unverified_header["kid"]:
+            rsa_key = {
+                "kty": key["kty"],
+                "kid": key["kid"],
+                "use": key["use"],
+                "n": key["n"],
+                "e": key["e"]
+            }
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=CLIENT_ID,
+                issuer="https://"+ DOMAIN+"/"
+            )
+        except jwt.ExpiredSignatureError:
+            raise AuthError({"code": "token_expired",
+                            "description": "token is expired"}, 401)
+        except jwt.JWTClaimsError:
+            raise AuthError({"code": "invalid_claims",
+                            "description":
+                                "incorrect claims,"
+                                " please check the audience and issuer"}, 401)
+        except Exception:
+            raise AuthError({"code": "invalid_header",
+                            "description":
+                                "Unable to parse authentication"
+                                " token."}, 401)
+
+        return payload
+    else:
+        return None
+
+# Generate a JWT from the Auth0 domain and return it
+# Request: JSON body with 2 properties with "username" and "password"
+#       of a user registered with this Auth0 domain
+# Response: JSON with the JWT as the value of the property id_token
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+
+
+@app.route('/')
+def index():
+    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+
+# Get or create food
+@app.route('/food', methods=['GET','POST'])
+def food():
+    if request.method == 'POST':
+        payload = verify_jwt(request)
+        if payload is None:
+            raise AuthError({"code": "no_rsa_key",
+                            "description":
+                                "No RSA key in JWKS"}, 401)
+        content = request.get_json()
+        new_food= datastore.entity.Entity(key=client.key(FOOD))
+        new_food.update({"name": content["name"], "price": content["price"],
+            "ingredients ": content["ingredients"]})
+        client.put(new_food)
+        new_food["id"] = new_food.key.id
+    return (json.dumps(new_food),201)
